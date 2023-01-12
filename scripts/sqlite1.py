31a59e163db3adbb29a11aed7514817467f14381
András Lászlók <106693780+AndrasLaszlok@users.noreply.github.com>
2023-01-12 15:53:25 +0100

    Scriptek feltöltése.

---
+++ scripts/sqlite1.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def insertData():
+    try:
+        sqliteConnection = sqlite3.connect('d:\Python\Portable Python-3.10.0 x64\Practice\pythondb\madaraszat.db')
+        cursor = sqliteConnection.cursor()
+        print("Csatlakozva az SQLite adatbázishoz")
+
+        sqlite_insert_query = """INSERT INTO madarak
+                          (id, faj, egyedszam, kozseg, kozseg_id, eov_e, eov_n)
+                          VALUES (15, 'Carduelis spinus', 2, 'Putnok', 623, 756085.3, 329337.8);"""
+
+        cursor.execute(sqlite_insert_query)
+        sqliteConnection.commit()
+        print("Összesen", cursor.rowcount, "Sikeres feltöltés")
+        sqliteConnection.commit()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Sikertelen feltöltés, hiba oka:", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("Adatbáziskapcsolat lezárult")
+
+insertData()
